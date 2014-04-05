3b2e131fae4abdd9ea538fa97aa2db3b1bd44953
Luc Olson <lusydo@gmail.com>
2014-04-05 14:20:47 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,67 @@
-
 $(document).ready(function(){
+	// var secretNumber = Math.floor(Math.random()*101),
+	// var difference = Math.abs(33 - secretNumber);
+	// console.log(difference);
 	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	var newGame = function() {
 
-  	});
+	};
+	var newNumber = Math.floor(Math.random()*100);
+	guessNum = [];
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+	$('#userGuess').on('input', function (event) {
+	    this.value = this.value.replace(/[^0-9]/g, '');
+	});
+	// $('#userGuess').isNumeric();
+	$('input[type="text"]').attr({ maxLength : 2 });
 
+	$('#guessButton').click(function() {
+		var guess = parseInt($('input[name="userGuess"]').val());
+		var difference = Math.abs(guess - newNumber);
+		if ( guess === newNumber ) {
+			$('#feedback').text('The Number is '+guess+' - You Win!!');
+			$('#guessButton').attr('value', 'New Game');
+			$('#guessButton').click(function() {
+			});
+		} else if ( difference <= 3 ) {
+			$('#feedback').text('WHITE HOT!');
+		} else if ( difference <= 5 ) {
+			$('#feedback').text('HOT HOT!');
+		} else if ( difference <= 10 ) {
+			$('#feedback').text('HOT.');
+		} else if ( difference <= 20 ) {
+			$('#feedback').text('WARM.');
+		} else if ( difference <= 40 ) {
+			$('#feedback').text('COLD.');
+		} else { $('#feedback').text('You are freezing to death..');
+		}
+
+		guessNum.push(guess);
+		$('input[type="text"]').val("");
+		
+		if (guessNum.length <= 12) {
+			$('#count').text(guessNum.length);
+			$('#guessList').append('<li>'+guess+'</li>');
+		} else {
+			$('#count').text(0);
+			$('#guessList').empty();
+			$('#feedback').text('You Lost. Try a New Game..');
+			newNumber = Math.floor(Math.random()*100);
+			guessNum = [];
+		}
+		console.log(newNumber);
+		console.log(guessNum);
+		event.preventDefault();
+	});
+
+	$('.new').click(function() {
+		newNumber = Math.floor(Math.random()*100);
+		guessNum = [];
+		$('#guessList').empty();
+		$('#count').text(0);
+		$('#feedback').text('New Game! Make a guess..');
+		// $('#guessButton').removeAttr("disabled"); 
+		console.log(newNumber);
+		console.log(guessNum);
+	});
 });
-
-
